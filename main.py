b224a49dd360c8d610a0cb390144df0bfbeea91c
Andrew Laptev <nickname.avast@gmail.com>
2021-10-28 17:56:21 +0300

    Add parsing graph from graph_symbolic.txt file

--- main.py
+++ main.py
@@ -1,33 +1,23 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 
+verts_arcs_map = {}
+
+with open("graph_symbolic.txt", "r") as file:
+    for line in file:
+        arr_str = line.split()
+        verts_arcs_map[arr_str[0]] = arr_str[1:]
+file.close()
+
 G = nx.Graph()
 
-G.add_edge('A251', 'B3', weight = 3)
-G.add_edge('A252', 'B2', weight = 2)
-G.add_edge('A253', 'B2', weight = 2)
-G.add_edge('A254', 'B1', weight = 3)
-G.add_edge('A255', 'B1', weight = 2)
-G.add_edge('A251', 'B4', weight = 2)
-G.add_edge('A252', 'B3', weight = 3)
-G.add_edge('A253', 'B3', weight = 2)
-G.add_edge('A254', 'B2', weight = 2)
-G.add_edge('A255', 'B2', weight = 3)
-G.add_edge('A251', 'B5', weight = 1)
-G.add_edge('A254', 'B3', weight = 4)
-G.add_edge('A258', 'B2', weight = 3)
-G.add_edge('A260', 'B2', weight = 2)
+for vert in verts_arcs_map:
+    for connection in verts_arcs_map[vert]:
+        arc_wght = connection.split(',')
+        G.add_edge(vert, arc_wght[0], weight = int(arc_wght[1]))
 
-path = nx.shortest_path(G, 'A251', 'A260', weight = 'weight')
+
+path = nx.shortest_path(G, 'A260', 'A251', weight = 'weight')
 path_edges = zip(path, path[1:])
 path_edges = set(path_edges) # add a set() path_edges = set(path_edges)after your zip() to get the shortest path color work
-# print(path)
-
-subax1 = plt.subplot()
-pos = nx.spring_layout(G)
-nx.draw_networkx(G, pos)
-labels = nx.get_edge_attributes(G, 'weight')
-nx.draw_networkx_edge_labels(G, pos, edge_labels = labels, font_size = 10)
-nx.draw_networkx_nodes(G, pos, nodelist = path, node_color = 'r')
-nx.draw_networkx_edges(G, pos, edgelist = path_edges, edge_color = 'r', width = 2)
-plt.savefig("graph.png")
+print(path)
