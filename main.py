996790732c57157ef3a59c3500fa371f7e919af1
Andrew Laptev <nickname.avast@gmail.com>
2021-11-10 18:44:47 +0300

    Add list of graph files

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 import floorpath as fp
 
-fp.init_graph("Kronva/graphs/14_floor.txt")
+files = [
+    "Kronva/graphs/floor_11.txt",
+    "Kronva/graphs/floor_12.txt",
+    "Kronva/graphs/floor_13.txt",
+    "Kronva/graphs/floor_14.txt",
+    "Kronva/graphs/floor_21.txt",
+    "Kronva/graphs/floor_22.txt",
+    "Kronva/graphs/floor_23.txt",
+    "Kronva/graphs/connect_floors.txt"
+]
+floor_graphs = []
+
+for filename in files:
+    floor_graphs.append(fp.init_graph(filename))
+
 # print(fp.fl_find_path("A251", "A275"))
-fp.show_graph()
+fp.show_graph(floor_graphs[7])
